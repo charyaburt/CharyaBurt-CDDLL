5c945074f18d03e3908a0db451489c9cbe4d0c6d
iamdamosuzuki <morganmorel@bavc.org>
2022-07-17 12:31:01 -0700

    updating config template for custom fieldnames

--- config_template.py
+++ config_template.py
@@ -6,6 +6,31 @@ BASE_ID = ""
 API_KEY = ""
 #TABLE_NAME = ""    # this isn't quite necessary anymore
 
+# Airtable Field References
+IN_LIBRARY = "[Deprc] In Library"
+IN_LIBRARY_LOOKUP = "[Lookup] In Library"
+RECORD_NUMBER = "[Formula] Record Number"
+RECORD_NUMBER_LOOKUP = "[Lookup] Record Number"
+FULL_FILE_NAME = "[KEY] Full File Name"
+CHECKSUM = "[Data] Checksum"
+FILE_PROCESS_STATUS = "[Mnt] File Processing Status"
+
+#Airtable File Metadata Fields
+FILENAME = "filename"
+DURATION = "duration"
+FILE_SIZE_STRING = "file size string"
+FILE_SIZE = "file size"
+FILE_FORMAT = "file format"
+VIDEO_CODEC = "video codec"
+VIDEO_BIT_DEPTH = "video bit depth"
+VIDEO_SCAN_TYPE = "video scan type"
+VIDEO_FRAME_RATE = "video frame rate"
+VIDEO_FRAME_SIZE = "video frame size"
+VIDEO_ASPECT_RATIO = "video aspect ratio"
+AUDIO_BIT_DEPTH = "audio bit depth"
+AUDIO_SAMPLING_RATE = "audio sampling rate"
+AUDIO_CODEC = "audio codec"
+
 # Vimeo Credentials
 YOUR_ACCESS_TOKEN = ""
 YOUR_CLIENT_ID = ""
